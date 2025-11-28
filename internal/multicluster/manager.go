6bb794b302fa6ec07eea61b4f72a44e4afc89008
vincenzo426 <c3nzosalv3mini@gmail.com>
2025-11-28 15:34:04 +0100

    new commit

--- internal/multicluster/manager.go
+++ internal/multicluster/manager.go
@@ -5,9 +5,9 @@ import (
 	"fmt"
 
 	corev1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/runtime"  // <-- AGGIUNGI QUESTA RIGA
+	"k8s.io/apimachinery/pkg/runtime" // <-- AGGIUNGI QUESTA RIGA
 	"k8s.io/apimachinery/pkg/types"
-	"k8s.io/client-go/rest"
+	//"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
